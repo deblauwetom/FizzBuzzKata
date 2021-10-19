de983c4f1447463d0d49ac5659f0b736f0a28a93
Tom Deblauwe <tom@toadi.com>
2021-10-20 00:55:07 +0200

    first try

--- test.cpp
+++ test.cpp
@@ -1,5 +1,97 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using namespace testing;
 using namespace std;
 
-TEST(bla, brol) {}
+class FBGame
+{
+public:
+    FBGame() {}
+
+    void start() {}
+
+    std::string nextAnswer()
+    {
+        ++currentAnswer;
+        if ((currentAnswer % 3) == 0) {
+            return "fizz";
+        }
+        if ((currentAnswer % 5) == 0) {
+            return "buzz";
+        }
+        return std::to_string(currentAnswer);
+    }
+
+private:
+    int currentAnswer{0};
+};
+
+class FBGameFixture : public Test
+{
+public:
+    FBGame game;
+
+    void incrementAnswer(int times)
+    {
+        for (auto i = 0; i < times; ++i) {
+            game.nextAnswer();
+        }
+    }
+};
+
+class FBGameStartedFixture : public FBGameFixture
+{
+public:
+    FBGameStartedFixture() { game.start(); }
+};
+
+TEST_F(FBGameFixture, GivenNewGame_WhenStarting_ThenStartAtOne)
+{
+    //Given
+
+    //When
+    game.start();
+
+    EXPECT_THAT(game.nextAnswer(), Eq("1"));
+}
+
+TEST_F(FBGameStartedFixture, WhenNextAnswer_ThenIncrementAnswerByOne)
+{
+    //When
+    game.nextAnswer();
+
+    //Then
+    EXPECT_THAT(game.nextAnswer(), Eq("2"));
+}
+
+TEST_F(FBGameStartedFixture, GivenNextAnswerDivisableBy3_WhenNextAnswer_ThenPrintFizz)
+{
+    incrementAnswer(2);
+
+    //when
+    auto next = game.nextAnswer();
+
+    //then
+    EXPECT_THAT(next, Eq("fizz"));
+}
+
+TEST_F(FBGameStartedFixture, GivenNextAnswerDivisableBy5_WhenNextAnswer_ThenPrintBuzz)
+{
+    incrementAnswer(4);
+
+    //when
+    auto next = game.nextAnswer();
+
+    //then
+    EXPECT_THAT(next, Eq("buzz"));
+}
+
+TEST(RealProgram, Output)
+{
+    FBGame game;
+    game.start();
+    for (auto i = 0; i < 100; ++i) {
+        std::cout << game.nextAnswer() << std::endl;
+    }
+}

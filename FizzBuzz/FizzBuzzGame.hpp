f9233688341f01f9e37c0f0b6a9cf037b5f75ad5
Tom Deblauwe <tom@toadi.com>
2021-10-20 06:45:55 +0200

    move to separate header file

--- FizzBuzz/FizzBuzzGame.hpp
+++ FizzBuzz/FizzBuzzGame.hpp
@@ -1,3 +1,69 @@
 #pragma once
 
-namespace katas {}
+#include <optional>
+#include <string>
+
+namespace katas {
+
+class FBGame {
+public:
+    FBGame() { }
+
+    void start() { }
+
+    std::string nextAnswer()
+    {
+        incrementAnswerCounter();
+
+        if (auto specialAnswer = checkSpecialAnswers()) {
+            return *specialAnswer;
+        } else {
+            return printAnswerCounter();
+        }
+    }
+
+private:
+    void incrementAnswerCounter()
+    {
+        ++currentAnswer;
+    }
+
+    bool isAnswerDivisableBy(int number)
+    {
+        return (currentAnswer % number) == 0;
+    }
+
+    void addWordToAnswerIfDivisableByOrHasGivenNumberInIt(int number, const std::string& word, std::string& answer)
+    {
+        if (isAnswerDivisableBy(number) || answerHasNumberInIt(number)) {
+            answer += word;
+        }
+    }
+
+    bool answerHasNumberInIt(int number)
+    {
+        auto printed = printAnswerCounter();
+        auto posOfNumber = printed.find(std::to_string(number));
+        return (posOfNumber != std::string::npos);
+    }
+
+    std::optional<std::string> checkSpecialAnswers()
+    {
+        std::string answer;
+        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(3, "Fizz", answer);
+        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(5, "Buzz", answer);
+        if (!answer.empty()) {
+            return answer;
+        }
+        return {};
+    }
+
+    std::string printAnswerCounter()
+    {
+        return std::to_string(currentAnswer);
+    }
+
+    int currentAnswer { 0 };
+};
+
+}
--- FizzBuzz/tests/FizzBuzzTests.cpp
+++ FizzBuzz/tests/FizzBuzzTests.cpp
@@ -1,69 +1,11 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "FizzBuzzGame.hpp"
+
 using namespace testing;
 using namespace std;
-
-class FBGame {
-public:
-    FBGame() { }
-
-    void start() { }
-
-    std::string nextAnswer()
-    {
-        incrementAnswerCounter();
-
-        if (auto specialAnswer = checkSpecialAnswers()) {
-            return *specialAnswer;
-        } else {
-            return printAnswerCounter();
-        }
-    }
-
-private:
-    void incrementAnswerCounter()
-    {
-        ++currentAnswer;
-    }
-
-    bool isAnswerDivisableBy(int number)
-    {
-        return (currentAnswer % number) == 0;
-    }
-
-    void addWordToAnswerIfDivisableByOrHasGivenNumberInIt(int number, const std::string& word, std::string& answer)
-    {
-        if (isAnswerDivisableBy(number) || answerHasNumberInIt(number)) {
-            answer += word;
-        }
-    }
-
-    bool answerHasNumberInIt(int number)
-    {
-        auto printed = printAnswerCounter();
-        auto posOfNumber = printed.find(std::to_string(number));
-        return (posOfNumber != std::string::npos);
-    }
-
-    std::optional<std::string> checkSpecialAnswers()
-    {
-        std::string answer;
-        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(3, "Fizz", answer);
-        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(5, "Buzz", answer);
-        if (!answer.empty()) {
-            return answer;
-        }
-        return {};
-    }
-
-    std::string printAnswerCounter()
-    {
-        return std::to_string(currentAnswer);
-    }
-
-    int currentAnswer { 0 };
-};
+using namespace katas;
 
 class FBGameFixture : public Test {
 public:

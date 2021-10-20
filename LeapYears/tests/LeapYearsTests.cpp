1b3e6b52db44c8d4f7ac0d4283a8575698d9099d
Tom Deblauwe <tom@toadi.com>
2021-10-20 07:05:30 +0200

    add 100 and 400 rule

--- LeapYears/tests/LeapYearsTests.cpp
+++ LeapYears/tests/LeapYearsTests.cpp
@@ -9,6 +9,11 @@ using namespace std;
 
 bool isLeapYear(int year)
 {
+    auto divisableBy100 = ((year % 100) == 0 ? true : false);
+    auto divisableBy400 = ((year % 400) == 0 ? true : false);
+    if (divisableBy100 && !divisableBy400) {
+        return false;
+    }
     return true;
 }
 
@@ -18,7 +23,21 @@ public:
 
 TEST_F(LeapYearsFixture, GivenYearDivisableBy400_ThenIsLeapYear)
 {
-    auto year = 1200;
+    auto year = 2000;
 
     EXPECT_TRUE(isLeapYear(year));
+}
+
+TEST_F(LeapYearsFixture, GivenYearDivisableBy100AndBy400_ThenIsLeapYear)
+{
+    auto year = 1600;
+
+    EXPECT_TRUE(isLeapYear(year));
+}
+
+TEST_F(LeapYearsFixture, GivenYearDivisableBy100ButNotBy400_ThenIsNotLeapYear)
+{
+    auto year = 1800;
+
+    EXPECT_FALSE(isLeapYear(year));
 }

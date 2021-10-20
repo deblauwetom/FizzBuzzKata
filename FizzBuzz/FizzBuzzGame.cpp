e2ea072997e09ecd71f127c5ba05565749817ff1
Tom Deblauwe <tom@toadi.com>
2021-10-20 06:48:41 +0200

    move impl to cpp file

--- FizzBuzz/FizzBuzzGame.cpp
+++ FizzBuzz/FizzBuzzGame.cpp
@@ -1,4 +1,59 @@
 #include "FizzBuzzGame.hpp"
 
 namespace katas {
+
+FBGame::FBGame() { }
+
+void FBGame::start() { }
+
+std::string FBGame::nextAnswer()
+{
+    incrementAnswerCounter();
+
+    if (auto specialAnswer = checkSpecialAnswers()) {
+        return *specialAnswer;
+    } else {
+        return printAnswerCounter();
+    }
+}
+
+void FBGame::incrementAnswerCounter()
+{
+    ++currentAnswer;
+}
+
+bool FBGame::isAnswerDivisableBy(int number)
+{
+    return (currentAnswer % number) == 0;
+}
+
+void FBGame::addWordToAnswerIfDivisableByOrHasGivenNumberInIt(int number, const std::string& word, std::string& answer)
+{
+    if (isAnswerDivisableBy(number) || answerHasNumberInIt(number)) {
+        answer += word;
+    }
+}
+
+bool FBGame::answerHasNumberInIt(int number)
+{
+    auto printed = printAnswerCounter();
+    auto posOfNumber = printed.find(std::to_string(number));
+    return (posOfNumber != std::string::npos);
+}
+
+std::optional<std::string> FBGame::checkSpecialAnswers()
+{
+    std::string answer;
+    addWordToAnswerIfDivisableByOrHasGivenNumberInIt(3, "Fizz", answer);
+    addWordToAnswerIfDivisableByOrHasGivenNumberInIt(5, "Buzz", answer);
+    if (!answer.empty()) {
+        return answer;
+    }
+    return {};
+}
+
+std::string FBGame::printAnswerCounter()
+{
+    return std::to_string(currentAnswer);
+}
 }
--- FizzBuzz/FizzBuzzGame.hpp
+++ FizzBuzz/FizzBuzzGame.hpp
@@ -7,61 +7,18 @@ namespace katas {
 
 class FBGame {
 public:
-    FBGame() { }
+    FBGame();
 
-    void start() { }
-
-    std::string nextAnswer()
-    {
-        incrementAnswerCounter();
-
-        if (auto specialAnswer = checkSpecialAnswers()) {
-            return *specialAnswer;
-        } else {
-            return printAnswerCounter();
-        }
-    }
+    void start();
+    std::string nextAnswer();
 
 private:
-    void incrementAnswerCounter()
-    {
-        ++currentAnswer;
-    }
-
-    bool isAnswerDivisableBy(int number)
-    {
-        return (currentAnswer % number) == 0;
-    }
-
-    void addWordToAnswerIfDivisableByOrHasGivenNumberInIt(int number, const std::string& word, std::string& answer)
-    {
-        if (isAnswerDivisableBy(number) || answerHasNumberInIt(number)) {
-            answer += word;
-        }
-    }
-
-    bool answerHasNumberInIt(int number)
-    {
-        auto printed = printAnswerCounter();
-        auto posOfNumber = printed.find(std::to_string(number));
-        return (posOfNumber != std::string::npos);
-    }
-
-    std::optional<std::string> checkSpecialAnswers()
-    {
-        std::string answer;
-        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(3, "Fizz", answer);
-        addWordToAnswerIfDivisableByOrHasGivenNumberInIt(5, "Buzz", answer);
-        if (!answer.empty()) {
-            return answer;
-        }
-        return {};
-    }
-
-    std::string printAnswerCounter()
-    {
-        return std::to_string(currentAnswer);
-    }
+    void incrementAnswerCounter();
+    bool isAnswerDivisableBy(int number);
+    void addWordToAnswerIfDivisableByOrHasGivenNumberInIt(int number, const std::string& word, std::string& answer);
+    bool answerHasNumberInIt(int number);
+    std::optional<std::string> checkSpecialAnswers();
+    std::string printAnswerCounter();
 
     int currentAnswer { 0 };
 };
